c6afdb5183f2f1d801f8ea96b4e06331b62a967d
Илья Антропов <127567149+Ilya-Antropov@users.noreply.github.com>
2025-11-02 23:06:18 +0700

    update 7 labs

--- 4st_Year_labs/low-level programming/7 labs.asm
+++ 4st_Year_labs/low-level programming/7 labs.asm
@@ -1,16 +1,13 @@
 section .data
-    prompt_n      db 'Enter n: ', 0
-    prompt_tern   db 'Enter ternary vector: ', 0
-    prompt_bool   db 'Enter boolean vector: ', 0
-    msg_yes       db 'Vector belongs to the interval.', 0xA, 0
-    msg_no        db 'Vector does not belong to the interval.', 0xA, 0
-    newline       db 0xA
+    prompt_ternary db "Enter ternary vector (32 symbols, 0/1/2): ", 0
+    prompt_bool    db "Enter boolean vector (32 symbols, 0/1): ", 0
+    yes_msg        db "Vector belongs to the interval", 0xA, 0
+    no_msg         db "Vector does not belong to the interval", 0xA, 0
+    newline        db 0xA
 
 section .bss
-    n         resd 1
-    ternary   resb 100
-    bool_vec  resb 100
-    temp      resb 100
+    ternary resb 33
+    bool_str resb 33
 
 section .text
     global _start
@@ -18,108 +15,93 @@ section .text
 _start:
     mov eax, 4
     mov ebx, 1
-    mov ecx, prompt_n
-    mov edx, 9
+    mov ecx, prompt_ternary
+    mov edx, 38
     int 0x80
 
-    call read_int
-    mov [n], eax
-
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, prompt_tern
-    mov edx, 22
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, ternary
+    mov edx, 33
     int 0x80
-
-    mov ecx, ternary     
-    call read_string
 
     mov eax, 4
     mov ebx, 1
     mov ecx, prompt_bool
-    mov edx, 23
+    mov edx, 35
     int 0x80
 
-    mov ecx, bool_vec
-    call read_string
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, bool_str
+    mov edx, 33
+    int 0x80
 
-    mov ecx, [n]        
-    xor ebx, ebx        
+    xor edi, edi       
+    xor esi, esi       
+    mov ecx, 0         
 
-check_loop:
-    mov al, [ternary + ebx] 
-    mov dl, [bool_vec + ebx]
+convert_ternary:
+    mov al, [ternary + ecx] 
+    cmp al, '0'
+    je set_mask
+    cmp al, '1'
+    je set_both
+    cmp al, '2'
+    je next_ternary 
+    jmp check_done
 
-    cmp al, '-'
-    je next_char
+set_mask:
+    bts edi, ecx    
+    jmp next_ternary
 
-    cmp al, dl
-    jne not_belong
+set_both:
+    bts edi, ecx
+    bts esi, ecx
 
-next_char:
-    inc ebx
-    loop check_loop
+next_ternary:
+    inc ecx
+    cmp ecx, 32
+    jl convert_ternary
 
+check_done:
+    xor ebx, ebx
+    mov ecx, 0
+
+convert_bool:
+    mov al, [bool_str + ecx]
+    cmp al, '1'
+    jne skip_set
+    bts ebx, ecx    
+
+skip_set:
+    inc ecx
+    cmp ecx, 32
+    jl convert_bool
+
+
+    mov eax, ebx
+    and eax, edi        
+    xor eax, esi        
+    test eax, edi       
+    jnz not_belongs
+
+belongs: 
     mov eax, 4
     mov ebx, 1
-    mov ecx, msg_yes
+    mov ecx, yes_msg
     mov edx, 28
     int 0x80
     jmp exit
 
-not_belong:
+not_belongs:
     mov eax, 4
     mov ebx, 1
-    mov ecx, msg_no
-    mov edx, 41
+    mov ecx, no_msg
+    mov edx, 36
     int 0x80
 
 exit:
     mov eax, 1
     xor ebx, ebx
     int 0x80
-
-read_int:
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, temp
-    mov edx, 10
-    int 0x80
-
-    xor eax, eax
-    mov ecx, temp
-convert_loop:
-    movzx edx, byte [ecx]
-    cmp dl, 0xA
-    je done_convert
-    imul eax, 10
-    sub edx, '0'
-    add eax, edx
-    inc ecx
-    jmp convert_loop
-done_convert:
-    ret
-
-read_string:
-    push ecx 
-    
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, temp
-    mov edx, 100
-    int 0x80
-
-    pop edi             
-    mov esi, temp       
-    mov ecx, eax        
-    dec ecx             
-    
-copy_loop:
-    mov al, [esi]
-    mov [edi], al
-    inc esi
-    inc edi
-    loop copy_loop
-    
-    mov byte [edi], 0   
-    ret
